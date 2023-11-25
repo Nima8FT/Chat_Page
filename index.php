c1990f3f84cb54ba1c79bf21ecec656f559599a9
Nima8FT <nima_8ak@yahoo.com>
2023-11-25 18:15:57 +0330

    The signup page is finished and the accounts have been added to the database

--- index.php
+++ index.php
@@ -14,30 +14,30 @@
   <div class="wrapper">
     <section class="form signup">
       <header>Chat Page</header>
-      <form action="#">
+      <form action="#" enctype="multipart/form-data">
         <div class="error-txt">this is an error message!</div>
         <div class="name-details">
           <div class="field input">
             <label>First Name</label>
-            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="First Name" name="fname" required/>
           </div>
           <div class="field input">
             <label>Last Name</label>
-            <input type="text" placeholder="Last Name" />
+            <input type="text" placeholder="Last Name" name="lname" required/>
           </div>
         </div>
         <div class="field input">
           <label>Email</label>
-          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Email" name="email" required/>
         </div>
         <div class="field input">
           <label>Password</label>
-          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Password" name="password" required/>
           <i class="fas fa-eye"></i>
         </div>
         <div class="field image">
           <label>Select Image</label>
-          <input type="file" />
+          <input type="file" name="images" required/>
         </div>
         <div class="field button">
           <input type="submit" value="continue to chat" />
---
+++ php/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$con = mysqli_connect("localhost","root","","chat");
+if(!$con) {
+    mysqli_connect_error();
+}
+
+?>
