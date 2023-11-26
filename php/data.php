ec10813d4d41c8a4ca2693601aaed9d79ec70c03
Nima8FT <nima_8ak@yahoo.com>
2023-11-26 16:07:31 +0330

    we did things like not having the person on the user page not scrolling to the top of the chat screen after each message and datermining wether each user is online or offline

--- php/data.php
+++ php/data.php
@@ -14,6 +14,7 @@ while ($row = mysqli_fetch_assoc($sql)) {
 
   (strlen($result) > 28) ? $msg = substr($result, 0, 28) . '...' : $msg = $result;
   ($outgoing_id == $row2['outgoing_msg_id'] ? $you = "You: " : $you = "");
+  ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
 
   $output .= '
     <a href="chat.php?user_id=' . $row['unique_id'] . '">
@@ -24,7 +25,7 @@ while ($row = mysqli_fetch_assoc($sql)) {
         <p>' . $you . $msg . '</p>
       </div>
     </div>
-    <div class="status-dot"><i class="fas fa-circle"></i></div>
+    <div class="status-dot ' . $offline . '"><i class="fas fa-circle"></i></div>
   </a>';
 }
 
--- php/users.php
+++ php/users.php
@@ -5,7 +5,7 @@ session_start();
 include_once("config.php");
 
 $outgoing_id = $_SESSION['unique_id'];
-$sql = mysqli_query($con, "SELECT * FROM users");
+$sql = mysqli_query($con, "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id}");
 $output = "";
 if (mysqli_num_rows($sql) == 0) {
   $output .= "No users are available to chat";
