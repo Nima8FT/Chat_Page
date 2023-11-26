f5aec1fff64db38b1519a04fba41222326f00232
Nima8FT <nima_8ak@yahoo.com>
2023-11-26 09:42:22 +0330

    we separated the header of the php pages

--- login.php
+++ login.php
@@ -1,40 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Chat Paege</title>
-    <link rel="stylesheet" href="./style.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-    />
-  </head>
-  <body>
-    <div class="wrapper">
-      <section class="form login">
-        <header>Chat Page</header>
-        <form action="#">
-          <div class="error-txt">this is an error message!</div>
-          <div class="field input">
-            <label>Email</label>
-            <input type="text" placeholder="Email" name="email"/>
-          </div>
-          <div class="field input">
-            <label>Password</label>
-            <input type="password" placeholder="Password" name="password"/>
-            <i class="fas fa-eye"></i>
-          </div>
-          <div class="field button">
-            <input type="submit" value="continue to chat" />
-          </div>
-        </form>
+<?php
+include_once("header.php");
+?>
 
-        <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
-      </section>
-    </div>
-  </body>
-  <script src="./javascript/pass-show-hide.js"></script>
-  <script src="./javascript/login.js"></script>
+<body>
+  <div class="wrapper">
+    <section class="form login">
+      <header>Chat Page</header>
+      <form action="#">
+        <div class="error-txt">this is an error message!</div>
+        <div class="field input">
+          <label>Email</label>
+          <input type="text" placeholder="Email" name="email" />
+        </div>
+        <div class="field input">
+          <label>Password</label>
+          <input type="password" placeholder="Password" name="password" />
+          <i class="fas fa-eye"></i>
+        </div>
+        <div class="field button">
+          <input type="submit" value="continue to chat" />
+        </div>
+      </form>
+
+      <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
+    </section>
+  </div>
+</body>
+<script src="./javascript/pass-show-hide.js"></script>
+<script src="./javascript/login.js"></script>
+
 </html>
--- users.php
+++ users.php
@@ -1,21 +1,10 @@
 <?php
 session_start();
+include_once("header.php");
 if (!isset($_SESSION['unique_id'])) {
   header("Location: login.php");
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Chat Paege</title>
-  <link rel="stylesheet" href="./style.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
-</head>
 
 <body>
   <div class="wrapper">
