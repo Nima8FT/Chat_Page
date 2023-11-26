d188fd0528e96feb26f4bfe13c657a2895c0ad9b
Nima8FT <nima_8ak@yahoo.com>
2023-11-26 10:49:47 +0330

    We created a list of users that displays all users

--- javascript/users.js
+++
@@ -1,8 +0,0 @@
-var searchBar = document.querySelector(".users .search input");
-var searchBtn =  document.querySelector(".users .search button");
-
-searchBtn.onclick = ()=> {
-    searchBar.classList.toggle("active");
-    searchBar.focus();
-    searchBtn.classList.toggle("active");
-}
--- users.php
+++ users.php
@@ -10,11 +10,22 @@ if (!isset($_SESSION['unique_id'])) {
   <div class="wrapper">
     <section class="users">
       <header>
+        <?php
+        include_once("php/config.php");
+        $sql = mysqli_query($con, "SELECT * FROM users WHERE unique_id= {$_SESSION['unique_id']}");
+        if (mysqli_num_rows($sql) > 0) {
+          $row = mysqli_fetch_assoc($sql);
+        }
+        ?>
         <div class="content">
-          <img src="img.jpg" alt="" />
+          <img src="php/images/<?php echo $row['img'] ?>" alt="" />
           <div class="details">
-            <span>Nima</span>
-            <p>Active</p>
+            <span>
+              <?php echo $row['fname'] . " " . $row['$lname'] ?>
+            </span>
+            <p>
+              <?php echo $row['status'] ?>
+            </p>
           </div>
         </div>
         <a href="#" class="logout">Logout</a>
@@ -25,86 +36,10 @@ if (!isset($_SESSION['unique_id'])) {
         <button><i class="fas fa-search"></i></button>
       </div>
       <div class="users-list">
-        <a href="#">
-          <div class="content">
-            <img src="img.jpg" alt="" />
-            <div class="details">
-              <span>Fateme</span>
-              <p>This it test message</p>
-            </div>
-          </div>
-          <div class="status-dot"><i class="fas fa-circle"></i></div>
-        </a>
-
-        <a href="#">
-          <div class="content">
-            <img src="img.jpg" alt="" />
-            <div class="details">
-              <span>Fateme</span>
-              <p>This it test message</p>
-            </div>
-          </div>
-          <div class="status-dot"><i class="fas fa-circle"></i></div>
-        </a>
-
-        <a href="#">
-          <div class="content">
-            <img src="img.jpg" alt="" />
-            <div class="details">
-              <span>Fateme</span>
-              <p>This it test message</p>
-            </div>
-          </div>
-          <div class="status-dot"><i class="fas fa-circle"></i></div>
-        </a>
-
-        <a href="#">
-          <div class="content">
-            <img src="img.jpg" alt="" />
-            <div class="details">
-              <span>Fateme</span>
-              <p>This it test message</p>
-            </div>
-          </div>
-          <div class="status-dot"><i class="fas fa-circle"></i></div>
-        </a>
-
-        <a href="#">
-          <div class="content">
-            <img src="img.jpg" alt="" />
-            <div class="details">
-              <span>Fateme</span>
-              <p>This it test message</p>
-            </div>
-          </div>
-          <div class="status-dot"><i class="fas fa-circle"></i></div>
-        </a>
-
-        <a href="#">
-          <div class="content">
-            <img src="img.jpg" alt="" />
-            <div class="details">
-              <span>Fateme</span>
-              <p>This it test message</p>
-            </div>
-          </div>
-          <div class="status-dot"><i class="fas fa-circle"></i></div>
-        </a>
-
-        <a href="#">
-          <div class="content">
-            <img src="img.jpg" alt="" />
-            <div class="details">
-              <span>Fateme</span>
-              <p>This it test message</p>
-            </div>
-          </div>
-          <div class="status-dot"><i class="fas fa-circle"></i></div>
-        </a>
       </div>
     </section>
   </div>
 </body>
-<script src="./javascript/users.js"></script>
+<script src="./javascript/user.js"></script>
 
 </html>
