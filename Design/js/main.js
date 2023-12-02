8cbc98518218a071716eac1781d4ad5bff259d42
Nima8FT <nima_8ak@yahoo.com>
2023-12-02 15:48:57 +0330

    making the password display section

--- Design/js/main.js
+++ Design/js/main.js
@@ -1,2 +1,15 @@
 $(document).ready(function () {
+
+  $("#eyes").click(function (e) {
+    var txt_type = $("#pass").attr("type");
+    if (txt_type == "password") {
+      $("#pass").attr("type", "text");
+      $("#eyes").addClass("active");
+    } else {
+      $("#pass").attr("type", "password");
+      $("#eyes").removeClass("active");
+    }
+  });
+
+  
 });
